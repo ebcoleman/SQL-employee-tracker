3e4b5f6f80d696ac5a923170a97119db3807ec89
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-16 16:07:39 -0500

    receiving error due to seeds.sql. need to revisit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
-INSERT INTO department (id, name)
-VALUES (001, 'Accounting'),
-    (002, 'Purchasing'),
-    (003, 'Production');
+INSERT INTO department (name)
+VALUES ('Accounting'),
+    ('Purchasing'),
+    ('Production');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (001, 'CFO', 100000, 111),
-    (002, 'Purchasing Manager', 85000, 222)
-    (003, 'Buyer', 60000, 222)
-    (004, 'Warehouse Supervisor', 65000, 333),
-    (005, 'Builder', 35000, 333);
+INSERT INTO role (title, salary, department_id)
+VALUES ('CFO', 100000, 1),  -- Change '001' to 1
+    ('Purchasing Manager', 85000, 2),  -- Change '002' to 2
+    ('Buyer', 60000, 2),  -- Change '002' to 2
+    ('Warehouse Supervisor', 65000, 3),  -- Change '003' to 3
+    ('Builder', 35000, 3);  -- Change '003' to 3
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES(001, 'Karen', 'Jones', 0002, null), 
-    (002, 'Adam', 'Jorge', 0020, 0002),
-    (003, 'Jason', 'Williams', 0021., 0020),
-    (004, 'Roy', 'Carter', 0030, 0002),
-    (005, 'Ruthie', 'Grey', 0031, 0030);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES('Karen', 'Jones', 1, null),  -- Change '001' to 1
+    ('Adam', 'Jorge', 2, 1),  -- Change '002' to 2
+    ('Jason', 'Williams', 3, 2),  -- Change '003' to 3
+    ('Roy', 'Carter', 4, 1),  -- Change '004' to 4
+    ('Ruthie', 'Grey', 5, 4);  -- Change '005' to 5
